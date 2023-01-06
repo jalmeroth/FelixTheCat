c827e3330d629cc60b915d4b3c7da91003e2d0b4
Jan Almeroth <jan@almeroth.com>
2023-01-06 10:50:25 +0100

    Better handle Eyes

--- upython/led.py
+++ upython/led.py
@@ -1,11 +1,28 @@
+"""Provide functions to control Eyes."""
 from machine import Pin
 from neopixel import NeoPixel
 
-NP = NeoPixel(Pin(14), 2)
+from const import COLORS, LED_COUNT, LED_PIN, LED_ORDER
 
 
-def set_eye_color(tup):
-    """Set eye's color."""
-    print(tup)
-    NP.fill(tup)
-    NP.write()
+class Eyes:
+    """Represent Eyes."""
+
+    def __init__(self) -> None:
+        self.neo = NeoPixel(Pin(LED_PIN), LED_COUNT)
+        self.neo.ORDER = LED_ORDER
+        self.show("white")
+
+    def set_color(self, color):
+        """Set eye's color."""
+        if isinstance(color, str):
+            color = COLORS.get(color)
+        print(color)
+        if color:
+            self.neo.fill(color)
+
+    def show(self, color=None):
+        """Show Eye color."""
+        if color:
+            self.set_color(color)
+        self.neo.write()
--- upython/main.py
+++ upython/main.py
@@ -1,15 +1,16 @@
 """Provice main routines."""
-from math import sin
 from machine import unique_id, Timer, reset
+from time import sleep
 from ubinascii import hexlify
 
 from umqtt_simple import MQTTClient, MQTTException
 
 from net import do_connect
-from led import set_eye_color
+from led import Eyes
 from config import Config
 from servo import wave
 
+EYES = Eyes()
 CONFIG = Config()
 COUNTER = 0
 CAT_NAME = getattr(CONFIG, "CAT_NAME", "FelixTheCat")
@@ -21,6 +22,7 @@ MQTT_CLIENT = MQTTClient(MQTT_CLIENT_ID, MQTT_SERVER, keepalive=MQTT_KEEPALIVE)
 
 MQTT_TOPIC_BASE = f"{CAT_NAME}"
 MQTT_TOPIC_STATUS = f"{MQTT_TOPIC_BASE}/connected"
+MQTT_TOPIC_ALLCATS = "winkekatze/allcats"
 
 tim = Timer(-1)
 tim.init(
@@ -31,26 +33,29 @@ tim.init(
 
 
 # Received messages from subscriptions will be delivered to this callback
-def sub_cb(topic, msg):
+def message_handler(topic, msg):
     """MQTT Callback."""
+    topic = topic.decode()
+    msg = msg.decode()
     print((topic, msg))
-    wink()
+    if topic == MQTT_TOPIC_STATUS:  # we successfully subscribed ourselfs
+        EYES.show("green")
+        sleep(1)
+        EYES.show("black")
+    elif topic == MQTT_TOPIC_ALLCATS or topic.endswith("command"):
+        wink()
+    elif topic.endswith("eye/set"):
+        EYES.set_color(msg)
+    else:
+        print("Meow.")
 
 
 def wink():
     """Wink."""
-    global COUNTER
-
-    r = int((1 + sin(COUNTER * 0.1324)) * 127)
-    g = int((1 + sin(COUNTER * 0.1654)) * 127)
-    b = int((1 + sin(COUNTER * 0.1)) * 127)
-
-    tup = (r, g, b)
-    COUNTER += 1
-
-    set_eye_color(tup)
+    print("^-^/")
+    EYES.show()
     wave()
-    set_eye_color((0, 0, 0))
+    EYES.show("black")
 
 
 def main():
@@ -58,11 +63,12 @@ def main():
     ssid = getattr(CONFIG, "WIFI_SSID")
     password = getattr(CONFIG, "WIFI_PASS")
     do_connect(ssid, password)
-    MQTT_CLIENT.set_callback(sub_cb)
+    EYES.show("yellow")
+    MQTT_CLIENT.set_callback(message_handler)
     MQTT_CLIENT.set_last_will(MQTT_TOPIC_STATUS, "0", True)
     MQTT_CLIENT.connect()
     MQTT_CLIENT.publish(MQTT_TOPIC_STATUS, "1", True)
-    MQTT_CLIENT.subscribe(b"winkekatze/allcats/#")
+    MQTT_CLIENT.subscribe(f"{MQTT_TOPIC_ALLCATS}/#".encode())
     MQTT_CLIENT.subscribe(f"{MQTT_TOPIC_BASE}/#".encode())
 
     while True:
