7afddb9f43f7ea5daead3a018d4a3112220dfad9
Jan Almeroth <jan@almeroth.com>
2023-01-09 11:06:23 +0100

    We don't need no AP-mode

--- upython/net.py
+++ upython/net.py
@@ -1,7 +1,17 @@
 import network
 
 
+def disable_ap_mode():
+    """Disable default AP-mode."""
+    ap_if = network.WLAN(network.AP_IF)
+    ap_if.active(False)
+    print("AP-mode disabled.")
+    del ap_if
+
+
 def do_connect(ssid, password):
+    """Connect to WiFi."""
+    disable_ap_mode()
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
